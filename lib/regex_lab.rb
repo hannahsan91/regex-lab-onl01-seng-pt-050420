a4c3aea1a815fef192cbddafe674b931ff224de8
Hannah Sandusky <hnnahsandusky@gmail.com>
2020-05-25 00:13:07 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,30 +1,33 @@
-class School
+def starts_with_a_vowel?(word)
+# (/[aeiou]/)
 
-  def initialize(school_name)
-    @school_name = school_name
-    @roster = {}
+  if word.match(/\A[aeiou]/i) == nil
+    return false
+  else
+    return true
   end
+end
 
-  def roster
-    @roster
+def words_starting_with_un_and_ending_with_ing(text)
+  text.scan(/\bun\w+ing\b/)
+end
+
+def words_five_letters_long(text)
+  text.scan(/\b\w{5}\b/)
+end
+
+def first_word_capitalized_and_ends_with_punctuation?(text)
+  if text.match(/^[A-Z].*\.$/) == nil
+    return false
+  else
+    return true
   end
+end
 
-  def add_student(name, grade)
-    @student_name = name
-    @grade = grade
-    if @roster.include?(grade) == false
-      @roster[grade] = []
-    end
-    @roster[grade] << name
-  end
-
-  def grade(number)
-    @roster[number]
-  end
-
-  def sort
-    @roster.each do |grade, name|
-      @roster[grade] = name.sort
-    end
+def valid_phone_number?(phone)
+  if phone.match(/\(?([0-9]{3})\)?([ -]?)([0-9]{3})([ -]?)([0-9]{4})/) == nil
+    return false
+  else
+    return true
   end
 end
